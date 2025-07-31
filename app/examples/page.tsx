3b3e1016c701a373343d04df0d0bdbe06e96334c
jgrey2550 <jgrey2550@gmail.com>
2025-07-30 22:01:34 -0700

    basic frontend including drag and drop field and navbar

---
+++ app/examples/page.tsx
@@ -0,0 +1,8 @@
+export default function ExamplesPage() {
+  return (
+    <div className="text-white p-8">
+      <h1 className="text-3xl font-bold mb-4">Examples Page</h1>
+      <p>Example videos n output will go here.</p>
+    </div>
+  );
+}
---
+++ app/requirements/page.tsx
@@ -0,0 +1,8 @@
+export default function RequirementsPage() {
+  return (
+    <div className="text-white p-8">
+      <h1 className="text-3xl font-bold mb-4">Requirements Page</h1>
+      <p>Video requirements here.</p>
+    </div>
+  );
+}
